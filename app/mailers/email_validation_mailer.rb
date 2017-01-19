4a8393d65119dca1655dafad034a5d11e198ff92
Alexandre Narbonne <alexandre.narbonne@gadz.org>
2017-01-19 11:01:12 +0100

    internationalize maillers

--- app/mailers/email_validation_mailer.rb
+++ app/mailers/email_validation_mailer.rb
@@ -4,7 +4,7 @@ class EmailValidationMailer < ApplicationMailer
     @user = user
     @era= era
     @confirmation_url= confirm_url
-    mail(to: @era.redirect, subject: 'Confirmation de ton adresse email')
+    mail(to: @era.redirect, subject: t('maillers.email_validation.confirm.subject'))
   end
 
   def notice_google_apps(user)
@@ -13,7 +13,7 @@ class EmailValidationMailer < ApplicationMailer
     not_borken_redirection = @user.email_redirect_accounts.where(type_redir: "smtp").select{|e| true unless e.broken?}.map(&:redirect)
 
     not_borken_redirection.each do |email|
-      mail(to: email, subject: 'Ton compte Google Gadz.org est créé')
+      mail(to: email, subject: t('maillers.email_validation.notice_google_apps.subject'))
     end
   end
 
